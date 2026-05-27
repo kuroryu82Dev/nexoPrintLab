df9cb79513e8cc35fbf86b9f8cae1972d2433d41
Roberto Almanza <almanzanr@gmail.com>
2026-05-27 12:29:55 -0600

    se corrige nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
-import ProductosContainer from './components/ProductosConteiner'
+import ProductosContainer from './components/ProductosConteiner';
 
 function App() {
   return (
     <>
     <Navbar />
-    <ProductosContainer />
+    <Routes>
+      <Route path="/inicio" element={<h1>Inicio</h1>} />
+      <Route path="/servicios" element={<h1>Servicios</h1>} />
+      <Route path="/productos" element={<ProductosContainer />} />
+      <Route path="/galeria" element={<h1>Galeria</h1>} />
+      <Route path="/cotizador" element={<h1>Cotizador</h1>} />
+      <Route path="/nosotros" element={<h1>Nosotros</h1>} />
+      <Route path="/contacto" element={<h1>Contacto</h1>} />
+    </Routes>
     </>
   )
 }
